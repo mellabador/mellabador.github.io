ca9f435052d380f57833d2efa2bf176ee8ff624d
mellabador <mei.melinda@bcchsnyc.net>
2017-07-19 15:59:25 -0400

    Update text_adventure.py

--- python_ad/text_adventure.py
+++ python_ad/text_adventure.py
@@ -1,19 +1,34 @@
+#start of story
+#context
 start = '''
-You wake up one morning and find that you aren't in your bed; you aren't even in your room.
-You're in the middle of a giant maze.
-A sign is hanging from the ivy: "You have one hour. Don't touch the walls."
-There is a hallway to your right and to your left.
+You're currently a high school student and it is time to consider working on your college application. 
+Do you want to start your college process early or later?
 '''
-
 
 print(start)
 
-
-print("Type 'left' to go left or 'right' to go right.")
+#recieve input: early or later
+print("Type 'early' to start early or 'later' to start later.")
 user_input = input()
-if user_input == "left":
-    print("You decide to go left and...") # finished the story by writing what happens
- 
-elif user_input == "right":
-    print("You choose to go right and ...") # finished the story writing what happens
- 
+
+
+#ouput of input: early or later
+if user_input == "early":
+	print("You decide to start your college process early. You are motivated! You have three choices: to research colleges, to visit school campuses, and to research your interests. How do you want to begin your process? ") 
+user_input = input("To research colleges, to visit school campuses or to research your interests?")
+if user_input == "to research colleges":
+	print("You have concluded the school you wanted to go to!")
+elif user_input == "to visit school campuses":
+	print("You realize you are one step closer to freedom!")
+elif user_input == "to research your interests":
+	print("You have found something you enjoy to do!")
+  
+elif user_input == "later":
+	print("You decide to start your college process later. You are bored and do not know what to do. What do you want to do?") 
+user_input = input("To hang out with your friends, to take a nap, or to go watch your shows on Netflix?")
+if user_input == "to hang out with your friends":
+	print ("Your day is wasted!")
+elif user_input == "to take a nap":
+	print ("You have slept more than you thought and it is the next day!")
+elif user_input == "to go watch your shows on Netflix":
+	print ("Many hours has passed and your day is over!")
